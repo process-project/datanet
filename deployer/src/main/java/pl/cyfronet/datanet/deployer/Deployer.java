0cf3c6c8ae677ea250f3ffe878bc69ab96cfb268
Daniel Harezlak <d.harezlak@scantech.com.pl>
2012-10-03 15:07:17 +0200

    - test framework dependency added - first test stub created for deployer

--- deployer/src/main/java/pl/cyfronet/datanet/deployer/Deployer.java
+++ deployer/src/main/java/pl/cyfronet/datanet/deployer/Deployer.java
@@ -1,13 +1,5 @@
 package pl.cyfronet.datanet.deployer;
 
 public class Deployer {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
---
+++ deployer/src/test/java/pl/cyfronet/datanet/deployer/test/DeploymentPerformance.java
@@ -0,0 +1,10 @@
+package pl.cyfronet.datanet.deployer.test;
+
+import org.junit.Test;
+
+public class DeploymentPerformance {
+	@Test
+	public void singleAppDeployment() {
+		//TODO
+	}
+}
