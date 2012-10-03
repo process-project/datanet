ab34c673f573dc77969983a409f99945b64b10c9
Daniel Harezlak <d.harezlak@scantech.com.pl>
2012-10-03 15:02:43 +0200

    deployer module created

---
+++ deployer/src/main/java/pl/cyfronet/datanet/deployer/Deployer.java
@@ -0,0 +1,13 @@
+package pl.cyfronet.datanet.deployer;
+
+public class Deployer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
