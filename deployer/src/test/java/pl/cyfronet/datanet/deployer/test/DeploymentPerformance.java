21bd4418502a321ed10a9dd2f4bb7ae41eeb3d9f
Daniel Harezlak <d.harezlak@scantech.com.pl>
2012-10-03 16:08:36 +0200

    client CF call implemented and working

--- deployer/src/test/java/pl/cyfronet/datanet/deployer/test/DeploymentPerformance.java
+++ deployer/src/test/java/pl/cyfronet/datanet/deployer/test/DeploymentPerformance.java
@@ -1,10 +1,38 @@
 package pl.cyfronet.datanet.deployer.test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.cloudfoundry.client.lib.CloudCredentials;
+import org.cloudfoundry.client.lib.CloudFoundryClient;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DeploymentPerformance {
+	private static final String CF_TARGET = System.getProperty("cf.target");
+	private static final String CF_USER = System.getProperty("cf.user");
+	private static final String CF_PASS = System.getProperty("cf.pass");
+	
+	private CloudFoundryClient client;
+	
+	@Before
+	public void setup() throws MalformedURLException {
+		client = new CloudFoundryClient(new CloudCredentials(CF_USER, CF_PASS), new URL(CF_TARGET));
+		client.login();
+	}
+	
+	@After
+	public void cleanup() {
+		if(client != null) {
+			client.logout();
+		}
+	}
+	
 	@Test
 	public void singleAppDeployment() {
-		//TODO
+		Assert.assertTrue(client.getCloudInfo() != null);
+		Assert.assertTrue(client.getApplications() != null);
 	}
 }
