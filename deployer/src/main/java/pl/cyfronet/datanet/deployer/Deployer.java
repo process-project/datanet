9c1f9e0dd8595f72d22eabcddc8fbfc17a12be51
Daniel Harezlak <d.harezlak@cyfronet.pl>
2012-12-17 08:04:08 +0100

    deployer's deploy method introduced

--- deployer/src/main/java/pl/cyfronet/datanet/deployer/Deployer.java
+++ deployer/src/main/java/pl/cyfronet/datanet/deployer/Deployer.java
@@ -1,5 +1,13 @@
 package pl.cyfronet.datanet.deployer;
 
+import java.util.List;
+
 public class Deployer {
+	public enum RepositoryType {
+		Mongo
+	}
 	
+	public void deployRepository(RepositoryType repositoryType, List<String> models) {
+		
+	}
 }
