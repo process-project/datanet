a9e1b4710a634d4c26530e820108f059c5bc0c0a
Maciej Pawlik <m.pawlik@cyfronet.pl>
2013-03-07 12:15:00 +0100

    Added code for serialization of EntityLists.

--- model/src/main/java/pl/cyfronet/datanet/model/beans/Entity.java
+++ model/src/main/java/pl/cyfronet/datanet/model/beans/Entity.java
@@ -34,6 +34,37 @@ public class Entity implements Serializable {
 	public void setFields(List<Field> fields) {
 		this.fields = fields;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fields == null) ? 0 : fields.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entity other = (Entity) obj;
+		if (fields == null) {
+			if (other.fields != null)
+				return false;
+		} else if (!fields.equals(other.fields))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
--- model/src/test/java/pl/cyfronet/datanet/model/test/JaxbTests.java
+++ model/src/test/java/pl/cyfronet/datanet/model/test/JaxbTests.java
@@ -1,24 +1,31 @@
 package pl.cyfronet.datanet.model.test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.xml.bind.JAXBException;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pl.cyfronet.datanet.model.beans.Entity;
 import pl.cyfronet.datanet.model.beans.Model;
+import pl.cyfronet.datanet.model.util.JaxbEntityListBuilder;
 import pl.cyfronet.datanet.model.util.ModelBuilder;
 
 public class JaxbTests {
 	private ModelBuilder modelBuilder;
+	private JaxbEntityListBuilder entitiesListBuilder;
 	
 	@Before
 	public void prepare() {
 		modelBuilder = new ModelBuilder();
+		entitiesListBuilder = new JaxbEntityListBuilder();
 	}
 	
 	@Test
-	public void testSerialization() throws JAXBException {
+	public void testSerializationOfModel() throws JAXBException {
 		String modelName = "Test name";
 		String modelVersion = "1.0";
 		Model model = new Model();
@@ -29,5 +36,23 @@ public class JaxbTests {
 		Model result = modelBuilder.deserialize(document);
 		Assert.assertEquals(modelName, result.getName());
 		Assert.assertEquals(modelVersion, result.getVersion());
+	}
+	
+	@Test
+	public void testSerializationOfEntities() throws JAXBException {
+		List<Entity> list = new LinkedList<Entity>();
+		
+		Entity e1 = new Entity();
+		e1.getFields().get(0).setName("wafle");
+		list.add(e1);
+		Entity e2 = new Entity();
+		e2.getFields().get(0).setName("wafle2");
+		list.add(e2);
+		
+		
+		String document = entitiesListBuilder.serialize(list);
+		List<Entity> result = entitiesListBuilder.deserialize(document);
+		
+		Assert.assertEquals(list, result);
 	}
 }
