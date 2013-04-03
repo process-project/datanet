a7b16b40ae2938233aa5d21e62400df22135b067
Maciej Pawlik <m.pawlik@cyfronet.pl>
2013-04-03 10:47:55 +0200

    Add unique part of app name in deployer test.

--- deployer/src/test/java/pl/cyfronet/datanet/deployer/DeployerTest.java
+++ deployer/src/test/java/pl/cyfronet/datanet/deployer/DeployerTest.java
@@ -3,8 +3,10 @@ package pl.cyfronet.datanet.deployer;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -20,10 +22,19 @@ public class DeployerTest extends CloudFoundryTest {
 	private static final String UNZIP_PATH = "/tmp/cloudfoundry-test-trash";
 	private static final String APP_FOLDER_NAME = "datanet-skel-mongodb";
 	
+	private final String repositoryName;
+	
 	private Deployer deployer;
 	
 	public DeployerTest() throws URISyntaxException, IOException  {
 		super();
+		Date date = new Date();
+		Random random = new Random();
+		
+		String uniqueComponent = String.format("_%d_%d", date.getTime(), Math.abs(random.nextInt()));
+		
+		repositoryName = REPOSITORY_NAME + uniqueComponent;
+		
 		File zip = new File(this.getClass().getClassLoader().getResource(ZIP_NAME).toURI());
 		Map <Deployer.RepositoryType, MapperBuilder> builderMap = new HashMap<Deployer.RepositoryType, MapperBuilder>();
 		builderMap.put(Deployer.RepositoryType.Mongo, new ZipFileMapperBuilder(zip, new File(UNZIP_PATH), APP_FOLDER_NAME));
@@ -39,13 +50,13 @@ public class DeployerTest extends CloudFoundryTest {
 	public void sampleDeploy() {
 		Map<String, String> models = new HashMap<String, String>();
 		models.put("entity123", "test{\"type\": \"object\"}");
-		deployer.deployRepository(Deployer.RepositoryType.Mongo, REPOSITORY_NAME, models);
+		deployer.deployRepository(Deployer.RepositoryType.Mongo, repositoryName, models);
 	}
 	
 	@Test
 	@Ignore
 	public void sampleUndeploy() {
-		deployer.undeployRepository(REPOSITORY_NAME);
+		deployer.undeployRepository(repositoryName);
 	}
 	
 	@Test
